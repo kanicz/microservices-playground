0d018641e9fcdc6faabca0c82a2af6da1c6d7345
Marcin Kania <mkania86@gmail.com>
2015-11-02 13:54:54 +0100

    Added empty webapp.

---
+++ webapp/src/main/java/com/futureprocessing/AwsMicroservicesPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.futureprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsMicroservicesPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsMicroservicesPlaygroundApplication.class, args);
+    }
+}
