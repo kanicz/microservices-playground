14c53c7b99f9dea13da6268b1abb846fa692f5b3
Marcin Kania <mkania86@gmail.com>
2015-11-17 11:18:28 +0100

    fixed setting active profiles

--- webapp/src/main/java/com/example/WebappController.java
+++ webapp/src/main/java/com/example/WebappController.java
@@ -1,7 +1,9 @@
 package com.example;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,11 @@ public class WebappController {
     @Value("${hostname:N/A}")
     private String hostname;
 
+    @Autowired
+    Environment env;
+
     @RequestMapping(value = "/")
     public ResponseEntity<String> rootResource() {
-        return new ResponseEntity<String>(welcomeMessage + " from host " + hostname, HttpStatus.OK);
+        return new ResponseEntity<String>(welcomeMessage + " from host " + hostname + " in profile " + env.getActiveProfiles()[0], HttpStatus.OK);
     }
 }
