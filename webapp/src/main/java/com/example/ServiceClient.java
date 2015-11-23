fee85428bcfaa7ea82222407f321c36c0800c893
Marcin Kania <mkania86@gmail.com>
2015-11-23 15:23:26 +0100

    fixes

--- webapp/src/main/java/com/example/ServiceClient.java
+++ webapp/src/main/java/com/example/ServiceClient.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("service")
-@FeignClient(name = "${feign.name}", url = "${feign.url}")
 public interface ServiceClient {
     @RequestMapping(method = RequestMethod.GET, value = "/welcome")
     String getWelcomeMessage();
